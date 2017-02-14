93bb7040530005fdbb15eec213e6d6beb72e576a
Jeremy Larose <jeremy.larose@protonmail.com>
2017-02-14 16:18:59 -0500

    Update schtasks.bat

--- salt-formula/files/win_pkg.notify/schtasks.bat
+++ salt-formula/files/win_pkg.notify/schtasks.bat
@@ -1,7 +1,7 @@
 pushd "%~dp0"
 
 :: add scheduled tasks
-schtasks /create /tn salt-win_pkg.notify /xml win_pkg.notify.xml /f
+schtasks /create /tn salt-win_pkg.notify /xml c:\salt\win_pkg.notify\win_pkg.notify.xml /f
 schtasks /Create /tn salt-win_pkg.notify-list_upgrades /ru "SYSTEM" /sc ONEVENT /MO "*[System[Provider[@Name='SaltMinion'] and EventID=125]]" /ec Application /rl HIGHEST /tr "c:\salt\salt-call pkg.list_upgrades | findstr /V /c:"local:" /c:"----------" /c:"latest" > c:\salt\win_pkg.notify\list_upgrades" /f
 schtasks /Create /tn salt-win_pkg.notify-state.apply /ru "SYSTEM" /sc ONEVENT /MO "*[System[Provider[@Name='SaltMinion'] and EventID=126]]" /ec Application /rl HIGHEST /tr "c:\salt\win_pkg.notify\state.apply_run.bat" /f
 schtasks /Create /tn salt-win_pkg.notify-pkg.upgrade /ru "SYSTEM" /sc ONEVENT /MO "*[System[Provider[@Name='SaltMinion'] and EventID=128]]" /ec Application /rl HIGHEST /tr "c:\salt\win_pkg.notify\pkg.upgrade_run.bat" /f
